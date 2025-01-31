ec3eb93c30b5c0172028e837d85af93b2f7a6d73
ashanephi <ash24013@byui.edu>
2025-01-31 22:14:35 +0100

    Worked on Week04 Assignment on the Youtube Class Design

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,19 @@
+public class Comment
+{
+    private string _firstName;
+    private string _lastName;
+    private string _comment;
+
+    public Comment(string firstName, string lastName, string comment)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _comment = comment;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"\t-{_firstName} {_lastName}: {_comment}");
+    }
+
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+        Video video1 = new Video("Intro to Programming", "Nephi Asha", 123000);
+
+        Comment comment1 = new Comment("John", "Doe", "This is a wonderful");
+        Comment comment2 = new Comment("Jason", "Friday", "This course is good to go over again");
+
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+
+        Video video2 = new Video("Learn CSS", "Ronald Regan", 34000);
+
+        Comment comment3 = new Comment("Nephi", "Asha", "This is a useless. He didn't even exlain the basics properly");
+        Comment comment4 = new Comment("Magnus", "Friday", "Hated it");
+
+        video2.AddComment(comment3);
+        video2.AddComment(comment4);
+
+        videos.Add(video1);
+        videos.Add(video2);
+
+        Console.WriteLine();
+
+        foreach(var video in videos)
+        {
+            video.DisplayVideoInfo();
+            Console.WriteLine();
+
+        }
+        
     }
 }
