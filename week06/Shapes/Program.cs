a816011e481d0c50cfa129bb2848e3013ff0b289
ashanephi <ash24013@byui.edu>
2025-02-13 01:08:15 +0100

    Shape Project done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square box = new Square("Purple", 4);
+
+        Rectangle rect = new Rectangle("Red", 6, 4);
+
+        Circle c = new Circle("Yello", 7);
+
+
+        shapes.Add(box);
+        shapes.Add(rect);
+        shapes.Add(c);
+
+        foreach(Shape sh in shapes)
+        {
+            DisplayShapeInformation(sh);
+            Console.WriteLine();
+        }
+
     }
+        public static void DisplayShapeInformation(Shape s)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}");
+        }
+
 }
